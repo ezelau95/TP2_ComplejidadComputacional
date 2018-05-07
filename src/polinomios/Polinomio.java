96fe2df82d1a1f5b78fc55be23c045ec66970ca5
Emmanuel <Emmanuel@Thermaltake>
2018-05-06 23:17:58 -0300

    Agrrgados los metodos recursivos y los test correspondientes

--- src/polinomios/Polinomio.java
+++ src/polinomios/Polinomio.java
@@ -2,41 +2,94 @@ package polinomios;
 
 public class Polinomio {
 	private int grado;
-	private double [] coeficientes;
-	
+	private double[] coeficientes;
+
 	public Polinomio(double[] c) {
 		coeficientes = c;
 		grado = c.length;
 	}
-	
-	//La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n y la posicion n contiene al termino independiente.
-	
-	//grado 4: 1x4 + 1x3 + 1x2 + 1x1 + 1
-	//		   0     1     2     3     4
-	// x = 1 : 
+
+	// La posicion 0 del arreglo de coeficientes contiene el coeficiente de grado n
+	// y la posicion n contiene al termino independiente.
+
+	// grado 4: 1x4 + 1x3 + 1x2 + 1x1 + 1
+	// 0 1 2 3 4
+	// x = 1 :
 	public double evaluarMSucesivas(double x) {
-		
+
 		double resultado = 0;
 		double aux;
 		for (int i = 0; i < grado; i++) {
 			aux = x;
-			for (int n = grado-i; n > 0; n--) {
-				aux *= x; 
+			for (int n = grado - i; n > 0; n--) {
+				aux *= x;
 			}
 			resultado += aux * coeficientes[i];
 		}
-		
+
 		return resultado;
 	}
-	
+
+	/**
+	 * Evalua el polinomio de manera recursiva Sin considerar si el exponente es par
+	 * o impar
+	 * 
+	 * @param x
+	 *            numero con el cual se va a evaluar el polinomio
+	 * @return el resultado del polinomio
+	 */
 	public double evaluarRecursiva(double x) {
-		return 0;
+		double resultado = 0;
+		for (int i = 0; i < this.grado; i++) {
+			resultado += this.coeficientes[i] * potencia(x, this.grado - i);
+		}
+		return resultado;
 	}
-	
+
+	private double potencia(double x, int n) {
+		if (n > 1)
+			return x * potencia(x, n - 1);
+		else if (n == 1)
+			return x;
+		else
+			return 1;
+	}
+
+	/**
+	 * Evalua el polinomio de manera recursiva considerando si el exponente es par o
+	 * impar
+	 * 
+	 * @param x
+	 *            numero con el cual se va a evaluar el polinomio
+	 * @return
+	 */
+	public double evaluarRecursivaPar(double x) {
+		double resultado = 0;
+		for (int i = 0; i < this.grado; i++) {
+			resultado += this.coeficientes[i] * potenciaParImpar(x, this.grado - i);
+		}
+		return resultado;
+	}
+
+	private double potenciaParImpar(double x, int n) {
+		
+		if (n > 1) {
+			if (n % 2 == 0) {
+				return potenciaParImpar(x * x, n / 2);
+			} else {
+				return x * potenciaParImpar(x, n - 1);
+			}
+		} else if (n == 1) {
+			return x;
+		} else {
+			return 1;
+		}
+	}
+
 	public double horner(double x) {
-        double result = 0;
-        for (int i = grado - 1; i >= 0; i--)
-            result = coeficientes[i] + (x * result);
-        return result;
-    }
+		double result = 0;
+		for (int i = grado - 1; i >= 0; i--)
+			result = coeficientes[i] + (x * result);
+		return result;
+	}
 }
--- test/polinomios/PolinomioTest.java
+++ test/polinomios/PolinomioTest.java
@@ -1,32 +1,53 @@
 package polinomios;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PolinomioTest {
-	
+
 	private Polinomio pol1;
 	private Polinomio pol2;
 	private Polinomio pol3;
-	
+
 	@Before
 	public void setUp() {
-		pol1 = new Polinomio(new double[] {1, 1, 1, 1, 1});
-		pol2 = new Polinomio(new double[] {2, 2, 2, 2, 2});
+		pol1 = new Polinomio(new double[] { 1, 1, 1, 1, 1 });
+		pol2 = new Polinomio(new double[] { 2, 2, 2, 2, 2 });
 	}
-	
+
 	@Test
-	public void evaluarMSucesivasTest(){
+	public void evaluarMSucesivasTest() {
 		double resultado = pol1.evaluarMSucesivas(1);
 		Assert.assertEquals(5, resultado, 0);
-		
+
 		resultado = pol1.evaluarMSucesivas(2);
-		Assert.assertEquals(31, resultado, 0);
-		
+		Assert.assertEquals(62, resultado, 0);
+
+	}
+
+	@Test
+	public void evaluarRecursivaTest() {
+		double resultado = pol1.evaluarRecursiva(1);
+		Assert.assertEquals(5, resultado, 0);
+
+		resultado = pol1.evaluarRecursiva(2);
+		Assert.assertEquals(62, resultado, 0);
+
 	}
 	
 	@Test
-	public void evaluarHorner(){
+	public void evaluarRecursivaParTest() {
+		double resultado = pol1.evaluarRecursivaPar(1);
+		Assert.assertEquals(5, resultado, 0);
+
+		resultado = pol1.evaluarRecursivaPar(2);
+		Assert.assertEquals(62, resultado, 0);
+
+	}
+
+	@Test
+	public void evaluarHorner() {
 		double resultado = pol1.horner(1);
 		Assert.assertEquals(5, resultado, 0);
 	}
