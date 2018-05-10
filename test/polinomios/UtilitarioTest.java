60e99675af8fb09e3e961192704668feed19bc76
Ezequiel Laurenti <ezee.laurenti@gmail.com>
2018-05-10 00:50:49 -0300

    Se completan test de Utilitario

--- test/polinomios/UtilitarioTest.java
+++ test/polinomios/UtilitarioTest.java
@@ -12,7 +12,6 @@ public class UtilitarioTest {
         Assert.assertEquals(Utilitario.factorial(2), 2);
         Assert.assertEquals(Utilitario.factorial(3), 6);
         Assert.assertEquals(Utilitario.factorial(6), 720);
-
     }
 
     @Test
@@ -21,7 +20,14 @@ public class UtilitarioTest {
         Assert.assertEquals(Utilitario.combinatoriaRecursiva(6, 1), 6);
         Assert.assertEquals(Utilitario.combinatoriaRecursiva(6, 2), 15);
         Assert.assertEquals(Utilitario.combinatoriaRecursiva(6, 6), 1);
-
+    }
+    
+    @Test
+    public void combinatoriaTest() {
+        Assert.assertEquals(Utilitario.combinatoria(6, 0), 1);
+        Assert.assertEquals(Utilitario.combinatoria(6, 1), 6);
+        Assert.assertEquals(Utilitario.combinatoria(6, 2), 15);
+        Assert.assertEquals(Utilitario.combinatoria(6, 6), 1);
     }
 
     @Test
@@ -29,5 +35,12 @@ public class UtilitarioTest {
         Assert.assertEquals(Math.pow(2, 5), Utilitario.potencia(2, 5), 100000000);
         Assert.assertEquals(Math.pow(5, 10), Utilitario.potencia(5, 10), 100000000);
         Assert.assertEquals(Math.pow(-17, 13), Utilitario.potencia(-17, 13), 100000000);
+    }
+    
+    @Test
+    public void potenciaParImparTest(){
+        Assert.assertEquals(Math.pow(2, 5), Utilitario.potenciaParImpar(2, 5), 100000000);
+        Assert.assertEquals(Math.pow(5, 10), Utilitario.potenciaParImpar(5, 10), 100000000);
+        Assert.assertEquals(Math.pow(-17, 13), Utilitario.potenciaParImpar(-17, 13), 100000000);
     }
 }
